a2505c6d9b501bc3f870f36c6215af5299df2f3f
VinithKrishnan <vinithk@gmail.com>
2017-02-14 22:52:28 +0530

    update1

--- interpreter.py
+++ interpreter.py
@@ -1,24 +1,16 @@
-# Token types
-#
-# EOF (end-of-file) token is used to indicate that
-# there is no more input left for lexical analysis
+
 INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
 
 
 class Token(object):
     def __init__(self, type, value):
-        # token type: INTEGER, PLUS, MINUS, or EOF
+
         self.type = type
-        # token value: non-negative integer value, '+', '-', or None
+
         self.value = value
 
     def __str__(self):
-        """String representation of the class instance.
 
-        Examples:
-            Token(INTEGER, 3)
-            Token(PLUS, '+')
-        """
         return 'Token({type}, {value})'.format(
             type=self.type,
             value=repr(self.value)
@@ -30,25 +22,22 @@ class Token(object):
 
 class Interpreter(object):
     def __init__(self, text):
-        # client string input, e.g. "3 + 5", "12 - 5 + 3", etc
+
         self.text = text
-        # self.pos is an index into self.text
+
         self.pos = 0
-        # current token instance
+
         self.current_token = None
         self.current_char = self.text[self.pos]
 
-    ##########################################################
-    # Lexer code                                             #
-    ##########################################################
     def error(self):
         raise Exception('Invalid syntax')
 
     def advance(self):
-        """Advance the `pos` pointer and set the `current_char` variable."""
+
         self.pos += 1
         if self.pos > len(self.text) - 1:
-            self.current_char = None  # Indicates end of input
+            self.current_char = None
         else:
             self.current_char = self.text[self.pos]
 
@@ -57,19 +46,15 @@ class Interpreter(object):
             self.advance()
 
     def integer(self):
-        """Return a (multidigit) integer consumed from the input."""
+
         result = ''
         while self.current_char is not None and self.current_char.isdigit():
             result += self.current_char
             self.advance()
         return int(result)
 
-    def get_next_token(self):
-        """Lexical analyzer (also known as scanner or tokenizer)
+    def lexical_analyser(self):
 
-        This method is responsible for breaking a sentence
-        apart into tokens. One token at a time.
-        """
         while self.current_char is not None:
 
             if self.current_char.isspace():
@@ -91,16 +76,10 @@ class Interpreter(object):
 
         return Token(EOF, None)
 
-    ##########################################################
-    # Parser / Interpreter code                              #
-    ##########################################################
     def eat(self, token_type):
-        # compare the current token type with the passed token
-        # type and if they match then "eat" the current token
-        # and assign the next token to the self.current_token,
-        # otherwise raise an exception.
+
         if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
+            self.current_token = self.lexical_analyser()
         else:
             self.error()
 
@@ -110,10 +89,10 @@ class Interpreter(object):
         self.eat(INTEGER)
         return token.value
 
-    def expr(self):
-        """Arithmetic expression parser / interpreter."""
-        # set current token to the first token taken from the input
-        self.current_token = self.get_next_token()
+    def parser(self):
+        """Arithmetic parseression parser / interpreter."""
+
+        self.current_token = self.lexical_analyser()
 
         result = self.term()
         while self.current_token.type in (PLUS, MINUS):
@@ -131,18 +110,16 @@ class Interpreter(object):
 def main():
     while True:
         try:
-            # To run under Python3 replace 'raw_input' call
-            # with 'input'
+
             text = raw_input('calc> ')
         except EOFError:
             break
         if not text:
             continue
         interpreter = Interpreter(text)
-        result = interpreter.expr()
+        result = interpreter.parser()
         print(result)
 
 
 if __name__ == '__main__':
     main()
-# Interpreter
